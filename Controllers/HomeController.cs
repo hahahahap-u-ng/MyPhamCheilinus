6c471ac8162a91e6e34ab704e3139ade45467e18
hahahahao-u-ng <hahahaphuong04@gmail.com>
2023-10-12 15:31:41 +0700

    12/10/23

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -3,6 +3,8 @@ using Microsoft.EntityFrameworkCore;
 using MyPhamCheilinus.Models;
 using System.Diagnostics;
 using X.PagedList;
+using static MyPhamCheilinus.Controllers.HomeController;
+
 namespace MyPhamCheilinus.Controllers
 {
 
@@ -50,24 +52,6 @@ namespace MyPhamCheilinus.Controllers
             // Lấy tên hãng tương ứng với mã hàng
             var tenHang = danhMucSanPhams.FirstOrDefault()?.MaHangNavigation?.TenHang;
 
-            // Sắp xếp danh sách sản phẩm dựa trên tham số sắp xếp
-            switch (sortOrder)
-            {
-                case "rating":
-                    danhMucSanPhams = danhMucSanPhams.OrderBy(x => x.DanhGia);
-                    break;
-                
-                case "price_high":
-                    danhMucSanPhams = danhMucSanPhams.OrderByDescending(x => x.Gia);
-                    break;
-                case "price_low":
-                    danhMucSanPhams = danhMucSanPhams.OrderBy(x => x.Gia);
-                    break;
-                default:
-                    danhMucSanPhams = danhMucSanPhams.OrderBy(x => x.TenDanhMuc);
-                    break;
-            }
-
             // Sử dụng thư viện PagedList để phân trang
             IPagedList<DanhMucSanPham> pagedList = await danhMucSanPhams.ToPagedListAsync(page ?? 1, pageSize);
 
@@ -78,6 +62,49 @@ namespace MyPhamCheilinus.Controllers
             ViewBag.TenHang = tenHang;
 
             return View(pagedList);
+        }
+
+        [HttpGet]
+        [HttpGet]
+        public IActionResult Filter(string mahang, List<string> selectedPrices, List<string> selectedCtLoais)
+        {
+            // Khởi tạo một truy vấn ban đầu với tất cả dữ liệu
+            var query = db.DanhMucSanPhams.AsQueryable();
+
+            // Áp dụng bộ lọc cho sản phẩm theo mã hàng
+            query = query.Where(d => d.MaHang == mahang);
+
+            // Áp dụng bộ lọc cho giá
+            if (selectedPrices != null && selectedPrices.Count > 0)
+            {
+                foreach (var priceRange in selectedPrices)
+                {
+                    if (priceRange == "all")
+                    {
+                        // Không áp dụng bộ lọc giá nếu chọn "Tất cả"
+                        continue;
+                    }
+
+                    // Chia chuỗi giá thành giá tối thiểu và tối đa
+                    var priceBounds = priceRange.Split('-');
+                    if (priceBounds.Length == 2 && double.TryParse(priceBounds[0], out double minPrice) && double.TryParse(priceBounds[1], out double maxPrice))
+                    {
+                        query = query.Where(d => d.Gia >= minPrice && d.Gia <= maxPrice);
+                    }
+                }
+            }
+
+            // Áp dụng bộ lọc cho Chi tiết loại (MaCtLoai)
+            if (selectedCtLoais != null && selectedCtLoais.Count > 0)
+            {
+                query = query.Where(d => selectedCtLoais.Contains(d.MaCtloai));
+            }
+
+            // Thực hiện truy vấn và lấy kết quả
+            var filteredData = query.ToList();
+
+            // Trả về kết quả dưới dạng PartialView
+            return PartialView("_ReturnHangs", filteredData);
         }
 
     }
